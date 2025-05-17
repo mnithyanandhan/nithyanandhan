8c2e9812cd74f64abc47d759dec07efda2750e20
mnithyanandhan <mnithyanandhan5@gmail.com>
2025-05-17 10:43:46 +0530

    cdm

---
+++ script.js
@@ -0,0 +1,23 @@
+var a=10
+a="Theeran"
+console.log(a)
+let b="react"
+
+function test(c,b){
+    var a=c+b
+    console.log(a)
+}
+console.log(b)
+test(10,2)
+test(12,10)
+
+var arr=[10,20,30,40];
+console.log(arr)
+arr[3]=70
+
+function check(){
+    for(i=0;i<=5;i++){
+        console.log(a)
+    }
+}
+check()
